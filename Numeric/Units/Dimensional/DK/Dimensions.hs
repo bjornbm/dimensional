1fa491f38e534d72f1767d07de19370cb645b9b0
Douglas McClean <douglas.mcclean@gmail.com>
2015-05-09 13:32:12 -0400

    Added arithmetic over term-level dimensions.

--- Numeric/Units/Dimensional/DK/Dimensions.hs
+++ Numeric/Units/Dimensional/DK/Dimensions.hs
@@ -1,5 +1,10 @@
 -- | Provides both term-level and type-level representations for physical dimensions in 
 -- a single import for convenience.
+--
+-- Presuming that users intend to work primarily with type level dimensions, this module hides
+-- arithmetic operators over term level dimensions and aliases for the base term-level dimensions
+-- to avoid namespace pollution. These features are available directly from
+-- "Numeric.Units.Dimensional.DK.Dimensions.TermLevel" if desired.
 module Numeric.Units.Dimensional.DK.Dimensions
 (
   module Numeric.Units.Dimensional.DK.Dimensions.TermLevel,
@@ -7,5 +12,5 @@ module Numeric.Units.Dimensional.DK.Dimensions
 )
 where
 
-import Numeric.Units.Dimensional.DK.Dimensions.TermLevel
+import Numeric.Units.Dimensional.DK.Dimensions.TermLevel hiding ((*), (/), recip, dOne, dLength, dMass, dTime, dElectricCurrent, dThermodynamicTemperature, dAmountOfSubstance, dLuminousIntensity)
 import Numeric.Units.Dimensional.DK.Dimensions.TypeLevel
--- Numeric/Units/Dimensional/DK/Dimensions/TermLevel.hs
+++ Numeric/Units/Dimensional/DK/Dimensions/TermLevel.hs
@@ -3,15 +3,25 @@ module Numeric.Units.Dimensional.DK.Dimensions.TermLevel
 (
   Dimension'(..),
   HasDimension(..),
+  dOne,
+  dLength, dMass, dTime, dElectricCurrent, dThermodynamicTemperature, dAmountOfSubstance, dLuminousIntensity,
+  (*), (/), recip,
   asList
 )
 where
+
+import Prelude hiding ((*), (/), recip)
 
 -- | A physical dimension, encoded as 7 integers, representing a factorization of the dimension into the
 -- 7 SI base dimensions. By convention they are stored in the same order as 
 -- in the 'Numeric.Units.Dimensional.DK.Dimensions.TypeLevel.Dimension' data kind.
 data Dimension' = Dim' !Int !Int !Int !Int !Int !Int !Int 
   deriving (Show, Eq, Ord)
+
+-- | The monoid of dimensions under multiplication.
+instance Monoid Dimension' where
+  mempty = dOne
+  mappend = (*)
 
 -- | Dimensional values inhabit this class, which allows access to a term-level representation of their dimension.
 class HasDimension a where 
@@ -20,6 +30,31 @@ class HasDimension a where
 
 instance HasDimension Dimension' where
   dimension = id
+
+-- | The dimension of dimensionless values.
+dOne :: Dimension'
+dOne = Dim' 0 0 0 0 0 0 0
+
+dLength, dMass, dTime, dElectricCurrent, dThermodynamicTemperature, dAmountOfSubstance, dLuminousIntensity :: Dimension'
+dLength                   = Dim' 1 0 0 0 0 0 0
+dMass                     = Dim' 0 1 0 0 0 0 0
+dTime                     = Dim' 0 0 1 0 0 0 0
+dElectricCurrent          = Dim' 0 0 0 1 0 0 0
+dThermodynamicTemperature = Dim' 0 0 0 0 1 0 0
+dAmountOfSubstance        = Dim' 0 0 0 0 0 1 0
+dLuminousIntensity        = Dim' 0 0 0 0 0 0 1
+
+-- | Forms the product of two dimensions.
+(*) :: Dimension' -> Dimension' -> Dimension'
+(Dim' l m t i th n j) * (Dim' l' m' t' i' th' n' j') = Dim' (l + l') (m + m') (t + t') (i + i') (th + th') (n + n') (j + j')
+
+-- | Forms the quotient of two dimensions.
+(/) :: Dimension' -> Dimension' -> Dimension'
+(Dim' l m t i th n j) / (Dim' l' m' t' i' th' n' j') = Dim' (l - l') (m - m') (t - t') (i - i') (th - th') (n - n') (j - j')
+
+-- | Forms the reciprocal of a dimension.
+recip :: Dimension' -> Dimension'
+recip = (dOne /)
 
 -- | Converts a dimension to a list of 7 integers, representing the exponent associated with each
 -- of the 7 SI base dimensions in the standard order.
