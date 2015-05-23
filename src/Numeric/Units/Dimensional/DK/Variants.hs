fe2e76822a7c9c5de31dd3e4733515d4985f4926
Douglas McClean <douglas.mcclean@gmail.com>
2015-05-23 00:16:09 -0400

    Improved documentation and exposed the Variants module.

--- src/Numeric/Units/Dimensional/DK/Variants.hs
+++ src/Numeric/Units/Dimensional/DK/Variants.hs
@@ -7,18 +7,31 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
+{- |
+   Copyright  : Copyright (C) 2006-2015 Bjorn Buckwalter
+   License    : BSD3
+
+   Maintainer : bjorn@buckwalter.se
+   Stability  : Stable
+   Portability: GHC only
+
+Provides a type level representation of 'Variant's of dimensional values,
+which may be quantities or units.
+-}
 module Numeric.Units.Dimensional.DK.Variants
 (
-  Metricality(..),
   type Variant(..),
+  Metricality(..),
   type (*), type Weaken
 )
 where
 
 import Data.Typeable
 
-data Metricality = Metric
-                 | NonMetric
+-- | Encodes whether a unit is a metric unit, that is, whether it can be combined
+-- with a metric prefix to form a related unit.
+data Metricality = Metric    -- ^ Capable of receiving a metric prefix.
+                 | NonMetric -- ^ Incapable of receiving a metric prefix.
   deriving (Eq, Ord, Typeable)
 
 {-
@@ -28,7 +41,9 @@ The phantom type variable v is used to distinguish between units
 and quantities. It must be one of the following:
 -}
 
-data Variant = DQuantity | DUnit Metricality
+-- | The kind of variants of dimensional values.
+data Variant = DQuantity         -- ^ The value is a quantity.
+             | DUnit Metricality -- ^ The value is a unit, possibly a 'Metric' unit.
   deriving (Eq, Ord, Typeable)
 
 {-
@@ -39,10 +54,16 @@ as the Prelude.
 
 infixl 7  *
 
+-- | Forms the product of two 'Variant's.
+--
+-- The product of units is a non-metric unit.
+--
+-- The product of quantities is a quantity.
 type family (v1 :: Variant) * (v2 :: Variant) :: Variant where
   'DUnit m1  * 'DUnit m2  = 'DUnit 'NonMetric
-  'DQuantity * 'DQuantity = 'DQuantity
+  'DQuantity * 'DQuantity = 'DQuantity 
 
+-- | Weakens a 'Variant' by forgetting possibly uninteresting type-level information.
 type family Weaken (v :: Variant) :: Variant where
   Weaken 'DQuantity = 'DQuantity
   Weaken ('DUnit m) = 'DUnit 'NonMetric
