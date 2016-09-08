6608c027612ae13fceccce91c13432cded5f1ee6
Douglas McClean <douglas.mcclean@gmail.com>
2016-09-07 22:56:05 -0400

    Added comment explaining the hspec autodiscovery magic.

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,1 +1,4 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+-- This module will automatically pull in all the Spec modules.
+-- See http://hspec.github.io/hspec-discover.html for a summary of how it works.
