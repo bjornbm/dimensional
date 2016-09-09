180242dcbebf33d88c315ff0c9bf7b037059d8e5
Douglas McClean <douglas.mcclean@gmail.com>
2016-09-09 15:02:50 -0400

    Moved comparison of static and dynamic implementations into the dynamic domain where it is less clutter and also does not allow accidental promotion of a polydimensional zero to a zero of some dimension that was hinted by the static side.

--- tests/Numeric/Units/Dimensional/DynamicSpec.hs
+++ tests/Numeric/Units/Dimensional/DynamicSpec.hs
@@ -63,7 +63,7 @@ spec = do
                phi' = demoteQuantity phi :: DynQuantity Double
            context "Num instance" $ do
              it "matches static addition" $ do
-               promoteQuantity (x1' P.+ x2') `shouldBe` Just (x1 + x2)
+               (x1' P.+ x2') `shouldBe` demoteQuantity (x1 + x2)
              it "allows addition with polydimensional zero" $ do
                (t' P.+ polydimensionalZero) `shouldBe` t'
                (polydimensionalZero P.+ t') `shouldBe` t'
@@ -78,7 +78,7 @@ spec = do
                    Just y = promoteQuantity (polydimensionalZero P.+ x) :: Maybe (Length Double)
                (y /~ meter) `shouldSatisfy` P.isNaN
              it "matches static subtraction" $ do
-               promoteQuantity (x2' P.- x1') `shouldBe` Just (x2 - x1)
+               (x2' P.- x1') `shouldBe` demoteQuantity (x2 - x1)
              it "allows subtraction with polydimensional zero" $ do
                (m' P.- polydimensionalZero) `shouldBe` m'
                (polydimensionalZero P.- m') `shouldBe` (P.negate m')
@@ -90,28 +90,28 @@ spec = do
                (polydimensionalZero P.* m') `shouldBe` polydimensionalZero
                (polydimensionalZero P.* polydimensionalZero) `shouldBe` (polydimensionalZero :: DynQuantity Double)
              it "matches static negation" $ do
-               promoteQuantity (P.negate m') `shouldBe` Just (negate m)
+               (P.negate m') `shouldBe` demoteQuantity (negate m)
              it "negates polydimensional zero" $ do
                (P.negate polydimensionalZero) `shouldBe` (polydimensionalZero :: DynQuantity Double)
              it "matches static absolute value" $ do
-               promoteQuantity (P.abs x2') `shouldBe` Just (abs x2)
+               (P.abs x2') `shouldBe` demoteQuantity (abs x2)
              it "takes absolute value of polydimensional zero" $ do
                (P.abs polydimensionalZero) `shouldBe` (polydimensionalZero :: DynQuantity Double)
              it "matches static signum" $ do
-               promoteQuantity (P.signum x1') `shouldBe` Just (signum x1)
-               promoteQuantity (P.signum x2') `shouldBe` Just (signum x2)
+               (P.signum x1') `shouldBe` demoteQuantity (signum x1)
+               (P.signum x2') `shouldBe` demoteQuantity (signum x2)
              it "takes signum of polydimensional zero" $ do
                (P.signum polydimensionalZero) `shouldBe` demoteQuantity (_0 :: Dimensionless Double)
              it "implements fromInteger with dimensionless result" $ do
-               promoteQuantity (P.fromInteger 7 :: DynQuantity Double) `shouldBe` Just _7
+               (P.fromInteger 7 :: DynQuantity Double) `shouldBe` demoteQuantity _7
            context "Fractional instance" $ do
              it "matches static division" $ do
-               promoteQuantity ((f' P.* x1') P./ t') `shouldBe` Just ((f * x1) / t)
+               ((f' P.* x1') P./ t') `shouldBe` demoteQuantity ((f * x1) / t)
              it "matches static reciprocal" $ do
-               promoteQuantity (P.recip m') `shouldBe` Just (recip m)
+               (P.recip m') `shouldBe` demoteQuantity (recip m)
              it "implements fromRational with dimensionless result" $ do
                let pi' = 22 P./ 7 :: Rational
-               promoteQuantity (P.fromRational pi' :: DynQuantity Rational) `shouldBe` Just (pi' *~ one)
+               (P.fromRational pi' :: DynQuantity Rational) `shouldBe` demoteQuantity (pi' *~ one)
              it "permits polydimensional zero as a dividend" $ do
                (polydimensionalZero P./ m') `shouldBe` polydimensionalZero
              it "does not permit polydimensional zero as a divisor" $ do
@@ -130,27 +130,27 @@ spec = do
                (y' /~ meter) `shouldSatisfy` P.isNaN
            context "Floating instance" $ do
              it "implements dimensionless pi" $ do
-               promoteQuantity (P.pi :: DynQuantity Double) `shouldBe` Just pi
+               (P.pi :: DynQuantity Double) `shouldBe` demoteQuantity pi
              it "implements dimensionless sin" $ do
                -- this will serve as a test for all the single-argument dimensionless functions
-               promoteQuantity (P.sin phi') `shouldBe` Just (sin phi)
+               (P.sin phi') `shouldBe` demoteQuantity (sin phi)
              it "rejects non-dimensionless arguments to sin" $ do
-               promoteQuantity (P.sin m') `shouldBe` (Nothing :: Maybe (Dimensionless Double))
+               (P.sin m') `shouldBe` invalidQuantity
              it "implements dimensionless sin of polydimensional zero" $ do
-               promoteQuantity (P.sin polydimensionalZero) `shouldBe` Just (_0 :: Dimensionless Double)
+               (P.sin polydimensionalZero) `shouldBe` (0 :: DynQuantity Double)
              it "matches static square root" $ do
-               promoteQuantity (P.sqrt a') `shouldBe` Just (sqrt a)
+               (P.sqrt a') `shouldBe` demoteQuantity (sqrt a)
              it "rejects arguments to square root with non-square dimensions" $ do
                (P.sqrt f') `shouldNotSatisfy` hasSomeDimension
              it "takes the square root of polydimensional zero" $ do
                (P.sqrt polydimensionalZero) `shouldBe` (polydimensionalZero :: DynQuantity Double)
              it "matches static dimensionless exponentiation" $ do
-               promoteQuantity (phi' P.** phi') `shouldBe` Just (phi ** phi)
+               (phi' P.** phi') `shouldBe` demoteQuantity (phi ** phi)
              it "rejects non-dimensionless arguments to dimensionless exponentiation" $ do
                (phi' P.** m') `shouldNotSatisfy` hasSomeDimension
                (x1' P.** phi') `shouldNotSatisfy` hasSomeDimension
              it "matches static logBase" $ do
-               promoteQuantity (P.logBase 10 phi') `shouldBe` Just (logBase (10 *~ one) phi)
+               (P.logBase 10 phi') `shouldBe` demoteQuantity (logBase (10 *~ one) phi)
              it "rejects non-dimensionless arguments to logBase" $ do
                (P.logBase 10 x1') `shouldNotSatisfy` hasSomeDimension
                (P.logBase x1' 10) `shouldNotSatisfy` hasSomeDimension
